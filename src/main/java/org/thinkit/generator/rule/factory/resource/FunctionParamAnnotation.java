cf61f878c903e5966a63b92b18c941140bfc3e89
Kato Shinya <yourdream28@gmail.com>
2020-05-29 14:34:25 +0900

    first commit

---
+++ src/main/java/org/thinkit/generator/rule/factory/resource/FunctionParamAnnotation.java
@@ -0,0 +1,68 @@
+/**
+ * Project Name : Generator<br>
+ * File Name : FunctionParamAnnotation.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/05/29<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.generator.rule.factory.resource;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * プログラムリソースにおける引数のアノテーション情報を抽象化した抽象クラスです。<br>
+ * この抽象クラスでは引数のアノテーション定義に必要な情報を保持します。<br>
+ * この抽象クラスを継承する具象クラスは必ず{@link Component#createResource()}を実装してください。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ * @see Component
+ * @see Component#createResource()
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public abstract class FunctionParamAnnotation implements Component {
+
+    /**
+     * 変数名
+     */
+    @NonNull
+    @Getter(AccessLevel.PROTECTED)
+    private String variableName = "";
+
+    /**
+     * 説明
+     */
+    @NonNull
+    @Getter(AccessLevel.PROTECTED)
+    private String description = "";
+
+    /**
+     * デフォルトコンストラクタ
+     */
+    @SuppressWarnings("unused")
+    private FunctionParamAnnotation() {
+    }
+
+    /**
+     * コンストラクタ
+     * 
+     * @param variableName 変数名
+     * @param description 説明
+     * @exception NullPointerException 引数として{@code null}が渡された場合
+     */
+    public FunctionParamAnnotation(String variableName, String description) {
+        this.variableName = variableName;
+        this.description = description;
+    }
+}
---
+++ src/main/java/org/thinkit/generator/rule/factory/resource/Resource.java
@@ -0,0 +1,69 @@
+/**
+ * Project Name : Generator<br>
+ * File Name : Resource.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/05/29<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.generator.rule.factory.resource;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * プログラムリソースを抽象化した抽象クラスです。<br>
+ * この抽象クラスではプログラムリソース定義に必要な情報を保持します。<br>
+ * この抽象クラスを継承する具象クラスは必ず{@link #createResource()}を実装してください。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public abstract class Resource {
+
+    /**
+     * リソース名
+     */
+    @NonNull
+    @Getter(AccessLevel.PROTECTED)
+    private String resourceName = "";
+
+    /**
+     * フィールド
+     */
+    @NonNull
+    @Getter(AccessLevel.PROTECTED)
+    private Field field = null;
+
+    /**
+     * 関数リスト
+     */
+    @NonNull
+    @Getter(AccessLevel.PROTECTED)
+    private List<Function> functions = null;
+
+    /**
+     * リソースを生成し文字列表現として返却する処理を定義するメソッドです。<br>
+     * このメソッドを定義している{@link Component}を継承した具象サブクラスは<br>
+     * 必ずこのメソッドを実装する必要があります。 <br>
+     * <br>
+     * この抽象メソッドはこの抽象クラスを継承する部品の特性に応じて実装してください。<br>
+     * 例えば、この抽象クラスをJavaリソースにおけるフィールド定義を管理する部品が継承した場合、<br>
+     * その部品クラスはフィールド定義を文字列として生成する処理を実装する必要があります。
+     * 
+     * @return 各要素を管理する部品で生成されたJavaリソース
+     */
+    public abstract String makeResource();
+}
