bf21fa0e18efdf7900ea2a1221491d899d84b27c
myConsciousness <yourdream28@gmail.com>
2020-06-04 21:38:41 +0900

    first commit

---
+++ src/main/java/org/thinkit/generator/rule/factory/dtofactory/DtoConstructor.java
@@ -0,0 +1,133 @@
+/**
+ * Project Name : Generator<br>
+ * File Name : DtoConstructor.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/06/04<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.generator.rule.factory.dtofactory;
+
+import java.util.List;
+
+import org.thinkit.generator.rule.factory.resource.Parameter;
+import org.thinkit.generator.rule.factory.resource.Process;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import org.thinkit.generator.rule.factory.resource.Function;
+import org.thinkit.generator.rule.factory.resource.FunctionDescription;
+
+/**
+ * DTOクラスのコンストラクタを生成する具象クラスです。<br>
+ * DTOに必要なコンストラクタを生成する処理を{@link Component#createResource()}に実装します。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ * @see Component
+ * @see Component#createResource()
+ * @see Function
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class DtoConstructor extends Function {
+
+    /**
+     * カンマ
+     */
+    private static final String COMMMA = ",";
+
+    /**
+     * アクセス修飾子
+     */
+    private static final String IDENTIFIER = "public";
+
+    /**
+     * 処理のインデント数
+     */
+    private static final int PROCESS_INDENT_COUNT = 8;
+
+    /**
+     * コンストラクタ
+     * 
+     * @param constructorName     コンストラクタ名
+     * @param functionDescription 関数の説明
+     * @exception NullPointerException 引数として{@code null}が渡された場合
+     */
+    public DtoConstructor(String constructorName, FunctionDescription functionDescription) {
+        super(constructorName, functionDescription);
+    }
+
+    @Override
+    public String createResource() {
+        final FunctionDescription functionDescription = super.getFunctionDescription();
+
+        final StringBuilder constructor = new StringBuilder();
+        final String indentSpaces = Indentation.getIndentSpaces();
+        final String indentSpace = Indentation.getSpace();
+        final String indentReturn = Indentation.getReturn();
+
+        constructor.append(functionDescription.createResource()).append(indentReturn);
+
+        constructor.append(indentSpaces).append(IDENTIFIER).append(indentSpace).append(super.getFunctionName());
+        constructor.append("(").append(this.toParameter()).append(")").append(indentSpace).append("{")
+                .append(indentReturn);
+        constructor.append(this.toProcess());
+        constructor.append("}");
+
+        return constructor.toString();
+    }
+
+    /**
+     * {@link Parameter}で設定された引数情報を文字列表現として返却します。<br>
+     * 引数情報が存在しない場合は必ず空文字列を返却します。
+     * 
+     * @return 引数の文字列表現
+     */
+    private String toParameter() {
+        final List<Parameter> parameters = super.getParameters();
+
+        if (parameters.isEmpty()) {
+            return "";
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        for (Parameter parameter : parameters) {
+            sb.append(parameter.createResource()).append(COMMMA).append(Indentation.getSpace());
+        }
+
+        sb.setLength(sb.length() - 1);
+        return sb.toString();
+    }
+
+    /**
+     * {@link Process}で設定された処理情報を文字列表現として返却します。<br>
+     * 処理情報が存在しない場合は必ず空文字列を返却します。
+     * 
+     * @return 処理の文字列表現
+     */
+    private String toProcess() {
+        List<Process> processes = super.getProcesses();
+
+        if (processes.isEmpty()) {
+            return "";
+        }
+
+        final StringBuilder sb = new StringBuilder();
+        final String indentSpaces = Indentation.getIndentSpaces(PROCESS_INDENT_COUNT);
+        final String indentReturn = Indentation.getReturn();
+
+        for (Process process : processes) {
+            sb.append(indentSpaces).append(process.createResource()).append(indentReturn);
+        }
+
+        sb.setLength(sb.length() - indentReturn.length());
+        return sb.toString();
+    }
+}
