e6ed5eb78173aecd9718784dc4b8d7b71d3f7284
myConsciousness <yourdream28@gmail.com>
2020-06-05 20:45:04 +0900

    first commit

---
+++ src/main/java/org/thinkit/generator/rule/factory/dtofactory/DtoResource.java
@@ -0,0 +1,53 @@
+/**
+ * Project Name : Generator<br>
+ * File Name : DtoResource.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/06/05<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.generator.rule.factory.dtofactory;
+
+import org.thinkit.generator.rule.factory.resource.Resource;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import org.thinkit.generator.rule.factory.resource.Field;
+
+/**
+ * DTOクラスのリソースを生成する具象クラスです。<br>
+ * DTOのリソースを生成する処理を{@link Component#createResource()}に実装します。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ * @see Component
+ * @see Component#createResource()
+ * @see Resource
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class DtoResource extends Resource {
+
+    /**
+     * コンストラクタ
+     * 
+     * @param resourceName リソース名
+     * @param field        フィールド定義
+     * 
+     * @exception NullPointerException 引数として{@code null}が渡された場合
+     */
+    public DtoResource(String resourceName, Field field) {
+        super(resourceName, field);
+    }
+
+    @Override
+    public String createResource() {
+        return "";
+    }
+}
