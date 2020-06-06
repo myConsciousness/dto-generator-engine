0463d21a87e4807f60a4917540ecf88aedcfa453
myConsciousness <yourdream28@gmail.com>
2020-06-06 20:59:11 +0900

    first commit

---
+++ src/main/java/org/thinkit/generator/rule/factory/dtofactory/DtoMethodDescription.java
@@ -0,0 +1,72 @@
+/**
+ * Project Name : Generator<br>
+ * File Name : DtoMethodDescription.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/06/06<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.generator.rule.factory.dtofactory;
+
+import org.thinkit.common.catalog.Indentation;
+import org.thinkit.generator.rule.factory.resource.FunctionDescription;
+
+import org.thinkit.generator.rule.factory.resource.FunctionParamAnnotation;
+import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * DTOクラスのメソッドの説明定義を生成する具象クラスです。<br>
+ * DTOに必要なメソッドの説明定義を生成する処理を{@link Component#createResource()}に実装します。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ * @see Component
+ * @see Component#createResource()
+ * @see FunctionDescription
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class DtoMethodDescription extends FunctionDescription {
+
+    /**
+     * コンストラクタ
+     * 
+     * @param description 説明
+     * @exception NullPointerException 引数として{@code null}が渡された場合
+     */
+    protected DtoMethodDescription(String description) {
+        super(description);
+    }
+
+    @Override
+    public String createResource() {
+        final String indentSpaces = Indentation.getIndentSpaces();
+        final String returnCode = Indentation.returnCode();
+
+        final StringBuilder methodDescription = new StringBuilder();
+        methodDescription.append(indentSpaces).append("/**").append(returnCode);
+        methodDescription.append(indentSpaces).append(" * ").append(super.getDescription()).append(returnCode);
+
+        if (super.hasParamAnnotation()) {
+            methodDescription.append(indentSpaces).append(" *").append(returnCode);
+
+            final List<FunctionParamAnnotation> functionParamAnnotations = super.getFunctionParamAnnotations();
+
+            for (FunctionParamAnnotation functionParamAnnotation : functionParamAnnotations) {
+                methodDescription.append(indentSpaces).append(" * ").append(functionParamAnnotation.createResource())
+                        .append(returnCode);
+            }
+        }
+
+        methodDescription.append(indentSpaces).append("*/");
+
+        return methodDescription.toString();
+    }
+}
---
+++ src/main/java/org/thinkit/generator/rule/factory/dtofactory/DtoMethodParamAnnotation.java
@@ -0,0 +1,59 @@
+/**
+ * Project Name : Generator<br>
+ * File Name : DtoMethodParamAnnotation.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/06/06<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.generator.rule.factory.dtofactory;
+
+import org.thinkit.common.catalog.Indentation;
+import org.thinkit.generator.catalog.dtogenerator.DtoAnnotation;
+import org.thinkit.generator.rule.factory.resource.FunctionParamAnnotation;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * DTOクラスのメソッドの引数アノテーションを生成する具象クラスです。<br>
+ * DTOに必要なメソッドの引数アノテーションを生成する処理を{@link Component#createResource()}に実装します。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ * @see Component
+ * @see Component#createResource()
+ * @see FunctionParamAnnotation
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class DtoMethodParamAnnotation extends FunctionParamAnnotation {
+
+    /**
+     * コンストラクタ
+     * 
+     * @param variableName 変数名
+     * @param description  説明
+     * @exception NullPointerException 引数として{@code null}が渡された場合
+     */
+    protected DtoMethodParamAnnotation(String variableName, String description) {
+        super(variableName, description);
+    }
+
+    @Override
+    public String createResource() {
+        final String space = Indentation.space();
+        final String annotation = DtoAnnotation.param();
+
+        final StringBuilder paramAnnotation = new StringBuilder();
+        paramAnnotation.append(annotation).append(space).append(super.getVariableName()).append(space)
+                .append(super.getDescription());
+
+        return paramAnnotation.toString();
+    }
+}
