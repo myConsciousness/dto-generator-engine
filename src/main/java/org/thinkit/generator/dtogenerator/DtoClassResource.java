605c4bc33781ad36893ed4d8aac47fa324f7c6a8
Kato Shinya <yourdream28@gmail.com>
2020-06-15 17:17:18 +0900

    first commit

---
+++ src/main/java/org/thinkit/generator/dtogenerator/DtoClassResource.java
@@ -0,0 +1,64 @@
+/**
+ * Project Name : Generator<br>
+ * File Name : DtoClassResource.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/06/15<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
+package org.thinkit.generator.dtogenerator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * DTOクラスのリソース情報を管理するデータクラスです。
+ * 
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class DtoClassResource {
+
+    /**
+     * パッケージ名
+     */
+    @NonNull
+    private String packageName = "";
+
+    /**
+     * リソースマップ
+     */
+    @NonNull
+    private Map<String, String> resources = new HashMap<>(0);
+
+    /**
+     * デフォルトコンストラクタ
+     */
+    @SuppressWarnings("unused")
+    private DtoClassResource() {
+    }
+
+    /**
+     * コンストラクタ
+     * 
+     * @param packageName パッケージ名
+     * @param resources   リソースマップ
+     */
+    public DtoClassResource(String packageName, Map<String, String> resources) {
+        this.packageName = packageName;
+        this.resources = resources;
+    }
+}
