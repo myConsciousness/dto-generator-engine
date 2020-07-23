2f4f68840020cc6710d59548c55b75ad67b5f1f2
Kato Shinya <yourdream28@gmail.com>
2020-07-23 16:22:59 +0900

    fixed

--- src/main/java/org/thinkit/generator/rule/factory/strategy/dtogenerator/DtoConstructorProcessContext.java
+++ src/main/java/org/thinkit/generator/rule/factory/strategy/dtogenerator/DtoConstructorProcessContext.java
@@ -1,5 +1,5 @@
 /**
- * Project Name : Generator<br>
+ * Project Name : generator-commons<br>
  * File Name : DtoConstructorProcessContext.java<br>
  * Encoding : UTF-8<br>
  * Creation Date : 2020/06/09<br>
