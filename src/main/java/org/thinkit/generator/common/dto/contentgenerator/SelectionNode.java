9e4ef6765ba243c629a6320e3f410f49e613b2b0
Kato Shinya <yourdream28@gmail.com>
2020-07-25 17:19:35 +0900

    apply fluent interface

--- src/main/java/org/thinkit/generator/common/dto/contentgenerator/SelectionNode.java
+++ src/main/java/org/thinkit/generator/common/dto/contentgenerator/SelectionNode.java
@@ -22,6 +22,18 @@ import lombok.ToString;
 
 /**
  * コンテンツにおける選択ノードの情報を管理するデータクラスです。
+ * <p>
+ * このクラスはFluentインターフェースの概念を応用し設計されています。<br>
+ * そのため、以下のようなメソッドチェーンでの操作が可能です。
+ *
+ * <pre>
+ * 使用例:
+ * <code>
+ * SelectionNode selectionNode = SelectionNode.of(conditionId)
+ *                                            .put(key1, value1)
+ *                                            .put(key2, value2);
+ * </code>
+ * </pre>
  *
  * @author Kato Shinya
  * @since 1.0
@@ -35,12 +47,12 @@ public final class SelectionNode {
     /**
      * 条件ID
      */
-    private String conditionId = "";
+    private String conditionId;
 
     /**
-     * 選択情報のセット
+     * 選択情報
      */
-    private Map<String, String> selection = new HashMap<>(0);
+    private Map<String, String> selection = Map.of();
 
     /**
      * デフォルトコンストラクタ
@@ -53,13 +65,23 @@ public final class SelectionNode {
      * コンストラクタ
      *
      * @param conditionId 条件ID
-     * @param selection   選択情報
      *
      * @exception NullPointerException 引数として {@code null} が渡された場合
      */
-    public SelectionNode(@NonNull String conditionId, @NonNull Map<String, String> selection) {
+    public SelectionNode(@NonNull String conditionId) {
         this.conditionId = conditionId;
-        this.selection = selection;
+    }
+
+    /**
+     * {@link SelectionNode} クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param conditionId 条件ID
+     * @return {@link SelectionNode} クラスの新しいインスタンス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public static SelectionNode of(@NonNull String conditionId) {
+        return new SelectionNode(conditionId);
     }
 
     /**
@@ -72,5 +94,30 @@ public final class SelectionNode {
     public SelectionNode(@NonNull SelectionNode selectionNode) {
         this.conditionId = selectionNode.getConditionId();
         this.selection = new HashMap<>(selectionNode.getSelection());
+    }
+
+    /**
+     * 引数として指定された {@code key} と {@code value} を選択情報に追加します。
+     * <p>
+     * この {@link SelectionNode#put(String, String)}
+     * メソッドは自分自身のインスタンスを返却するため以下のようなメソッドチェーンでの操作が可能です。
+     *
+     * <pre>
+     * <code>
+     * SelectionNode selectionNode = SelectionNode.of(conditionId)
+     *                                           .put(key1, value1)
+     *                                           .put(key2, value2);
+     * </code>
+     * </pre>
+     *
+     * @param key   選択情報のキー
+     * @param value 選択情報の値
+     * @return 自分自身のインスタンス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public SelectionNode put(@NonNull String key, @NonNull String value) {
+        selection.put(key, value);
+        return this;
     }
 }
