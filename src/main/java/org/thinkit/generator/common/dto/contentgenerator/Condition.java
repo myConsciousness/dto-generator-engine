820590e51ae58748aed818228985f82416ab81ad
Kato Shinya <yourdream28@gmail.com>
2020-07-26 08:55:35 +0900

    add static constructor

--- src/main/java/org/thinkit/generator/common/dto/contentgenerator/Condition.java
+++ src/main/java/org/thinkit/generator/common/dto/contentgenerator/Condition.java
@@ -1,3 +1,15 @@
+/**
+ * Project Name : generator-commons<br>
+ * File Name : Condition.java<br>
+ * Encoding : UTF-8<br>
+ * Creation Date : 2020/07/25<br>
+ * <p>
+ * Copyright © 2020 Kato Shinya. All rights reserved.
+ * <p>
+ * This source code or any portion thereof must not be<br>
+ * reproduced or used in any manner whatsoever.
+ */
+
 package org.thinkit.generator.common.dto.contentgenerator;
 
 import lombok.EqualsAndHashCode;
@@ -5,6 +17,13 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.ToString;
 
+/**
+ * コンテンツの条件を管理スデータクラスです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
 @Getter
 @ToString
 @EqualsAndHashCode
@@ -53,5 +72,32 @@ public final class Condition {
         this.key = condition.getKey();
         this.operand = condition.getOperand();
         this.value = condition.getValue();
+    }
+
+    /**
+     * 引数として渡された情報を基に {@link Condition} クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param key     キー
+     * @param operand 演算子
+     * @param value   値
+     * @return {@link Condition} クラスの新しいインスタンス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public static Condition of(@NonNull String key, @NonNull String operand, @NonNull String value) {
+        return new Condition(key, operand, value);
+    }
+
+    /**
+     * 引数として渡された {@code condition} オブジェクトの情報を基に {@link Condition}
+     * クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param condition 条件オブジェクト
+     * @return {@link Condition} クラスの新しいインスタンス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public static Condition of(@NonNull Condition condition) {
+        return new Condition(condition);
     }
 }
