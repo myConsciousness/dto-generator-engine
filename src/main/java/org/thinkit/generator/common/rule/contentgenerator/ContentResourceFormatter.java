5d3907d8236dfb9cf2861a74fc0a80221687278c
Kato Shinya <yourdream28@gmail.com>
2020-07-26 14:46:16 +0900

    add

---
+++ src/main/java/org/thinkit/generator/common/rule/contentgenerator/ContentResourceFormatter.java
@@ -0,0 +1,11 @@
+package org.thinkit.generator.common.rule.contentgenerator;
+
+import org.thinkit.common.rule.AbstractRule;
+
+public final class ContentResourceFormatter extends AbstractRule {
+
+    @Override
+    public boolean execute() {
+        return true;
+    }
+}
