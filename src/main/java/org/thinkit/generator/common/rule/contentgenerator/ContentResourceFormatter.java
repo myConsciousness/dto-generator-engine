21770e7c82f46493d17278cb3a5dac6901f57d11
Kato Shinya <yourdream28@gmail.com>
2020-07-26 14:56:26 +0900

    update

--- src/main/java/org/thinkit/generator/common/rule/contentgenerator/ContentResourceFormatter.java
+++ src/main/java/org/thinkit/generator/common/rule/contentgenerator/ContentResourceFormatter.java
@@ -1,8 +1,58 @@
 package org.thinkit.generator.common.rule.contentgenerator;
 
 import org.thinkit.common.rule.AbstractRule;
+import org.thinkit.generator.common.dto.contentgenerator.ContentMatrix;
+import org.thinkit.generator.common.dto.contentgenerator.ContentResource;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode(callSuper = false)
 public final class ContentResourceFormatter extends AbstractRule {
+
+    /**
+     * コンテンツマトリクス
+     */
+    private ContentMatrix contentMatrix;
+
+    /**
+     * コンテンツリソース
+     */
+    @Getter
+    private ContentResource contentResource;
+
+    /**
+     * デフォルトコンストラクタ
+     */
+    private ContentResourceFormatter() {
+    }
+
+    /**
+     * コンストラクタ
+     *
+     * @param contentMatrix コンテンツリソースへ整形する情報を格納したコンテンツマトリクス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    private ContentResourceFormatter(@NonNull ContentMatrix contentMatrix) {
+        this.contentMatrix = contentMatrix;
+    }
+
+    /**
+     * 引数として渡された {@code contentMatrix} オブジェクトをもとに {@link ContentResourceFormatter}
+     * クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param contentMatrix コンテンツリソースへ整形する情報を格納したコンテンツマトリクス
+     * @return {@link ContentResourceFormatter} クラスの新しいインスタンス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public static ContentResourceFormatter of(@NonNull ContentMatrix contentMatrix) {
+        return new ContentResourceFormatter(contentMatrix);
+    }
 
     @Override
     public boolean execute() {
