c86207338cfb4ab7915c3b9ec0acacae046b0b95
Kato Shinya <yourdream28@gmail.com>
2020-08-01 10:04:53 +0900

    fixed

--- src/main/java/org/thinkit/generator/common/vo/content/ContentResource.java
+++ src/main/java/org/thinkit/generator/common/vo/content/ContentResource.java
@@ -36,34 +36,82 @@ public final class ContentResource implements Serializable {
     private static final long serialVersionUID = 5913289046847116788L;
 
     /**
+     * パッケージ名
+     */
+    @Getter
+    private String packageName;
+
+    /**
      * コンテンツ名
      */
     @Getter
-    private String contentName = "";
+    private String contentName;
 
     /**
      * コンテンツ
      */
     @Getter
-    private String content = "";
+    private String content;
 
     /**
      * デフォルトコンストラクタ
      */
-    @SuppressWarnings("unused")
     private ContentResource() {
     }
 
     /**
      * コンストラクタ
      *
+     * @param packageName パッケージ名
      * @param contentName コンテンツ名
      * @param content     コンテンツ
      *
      * @exception NullPointerException 引数として {@code null} が渡された場合
      */
-    public ContentResource(@NonNull String contentName, @NonNull String content) {
+    private ContentResource(@NonNull String packageName, @NonNull String contentName, @NonNull String content) {
+        this.packageName = packageName;
         this.contentName = contentName;
         this.content = content;
+    }
+
+    /**
+     * コピーコンストラクタ
+     *
+     * @param contentResource コンテンツリソース
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    private ContentResource(@NonNull ContentResource contentResource) {
+        this.packageName = contentResource.getPackageName();
+        this.contentName = contentResource.getContentName();
+        this.content = contentResource.getContent();
+    }
+
+    /**
+     * 引数として渡された情報を基に {@link ContentResource} クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param packageName パッケージ名
+     * @param contentName コンテンツ名
+     * @param content     コンテンツ
+     * @return {@link ContentResource} クラスの新しいインスタンス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public static ContentResource of(@NonNull String packageName, @NonNull String contentName,
+            @NonNull String content) {
+        return new ContentResource(packageName, contentName, content);
+    }
+
+    /**
+     * 引数として渡された {@code contentResource} オブジェクトの情報を基に {@link ContentResource}
+     * クラスの新しいインスタンスを生成し返却します。
+     *
+     * @param contentResource コンテンツリソース
+     * @return {@link ContentResource} クラスの新しいインスタンス
+     *
+     * @exception NullPointerException 引数として {@code null} が渡された場合
+     */
+    public static ContentResource of(@NonNull ContentResource contentResource) {
+        return new ContentResource(contentResource);
     }
 }
