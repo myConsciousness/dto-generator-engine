04db4fc92011da874c6e40204c40ca70924b3f98
Kato Shinya <yourdream28@gmail.com>
2020-10-17 16:05:58 +0900

    update import

--- src/main/java/org/thinkit/generator/common/catalog/JsonNodeType.java
+++ src/main/java/org/thinkit/generator/common/catalog/JsonNodeType.java
@@ -14,7 +14,7 @@
 
 package org.thinkit.generator.common.catalog;
 
-import org.thinkit.common.catalog.Catalog;
+import org.thinkit.api.catalog.Catalog;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
